6288bd639081cbf7f25f78fd36af40e58244da1b
jeen <neej.cool@gmail.com>
2020-09-05 11:24:37 +0530

    Adding product com and format currency

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-//feauture 1
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.css";
 import App from './app';
-
-//const element = <h1 class="container">Hello World</h1>;
 
 ReactDOM.render(<App/>,document.getElementById('root'));
